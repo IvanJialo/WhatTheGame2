e6673b17262dd5548ebc3f5c2e80354420126bb4
Kiwi <ivajimalo@alu.edu.gva.es>
2025-01-07 19:57:34 +0100

    HeroBanner and HomeView

---
+++ src/components/HeroBanner.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="bg-white/60 backdrop-blur dark:border-gray-600 dark:bg-black/60 text-white mt-[64px]">
+      <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center">
+        <div class="mx-auto max-w-3xl text-center">
+          <h1
+            class="bg-gradient-to-r from-[#DCD0FF] via-blue-500 to-[#b197ff] bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+          >
+            Understand User Flow.
+    
+            <span class="sm:block"> Increase Conversion. </span>
+          </h1>
+    
+          <p class="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+            numquam ea!
+          </p>
+    
+          <div class="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+              class="block w-full rounded border border-[#DCD0FF] bg-[#DCD0FF] px-12 py-3 text-sm font-medium text-black hover:bg-transparent hover:text-white focus:outline-none active:text-opacity-75 sm:w-auto"
+              href="#"
+            >
+              Get Started
+            </a>
+    
+            <a
+              class="block w-full rounded border border-[#b197ff] px-12 py-3 text-sm font-medium text-white hover:bg-[#b197ff] hover:text-black focus:outline-none active:bg-opacity-75 sm:w-auto "
+              href="#"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+import { RouterLink, RouterView } from 'vue-router'
+import HeroBanner from '@/components/HeroBanner.vue';
 </script>
 
 <template>
   <main>
-    
+    <HeroBanner />
   </main>
 </template>
