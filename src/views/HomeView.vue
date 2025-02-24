1d99756e64cfc71c04840273aac599f42563ba1f
Kiwi <ivajimalo@alu.edu.gva.es>
2025-02-24 15:29:18 +0100

    vuelta a cambiar el imput del home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,6 @@ const searchQuery = ref("");
 const allGames = ref([]);
 const randomGames = ref([]);
 
-// Cargar todos los juegos desde Firebase.
 const loadGamesFromFirebase = () => {
   const gamesRef = dbRef(database, "categories");
 
@@ -23,7 +22,6 @@ const loadGamesFromFirebase = () => {
   });
 };
 
-// Generar 5 juegos aleatorios.
 const getRandomGames = () => {
   const shuffled = [...allGames.value].sort(() => 0.5 - Math.random());
   randomGames.value = shuffled.slice(0, 5);
@@ -36,21 +34,17 @@ onMounted(() => {
 
 <template>
   <main class="bg-white/50 dark:bg-black/70">
-    <HeroBanner class="animate-blurred-fade-in"/>
-    <SearchInput v-model="searchQuery" class="animate-blurred-fade-in mt-8 max-w-[80%] sm:max-w-[60%] xl:max-w-[30%] mb-44 sm:mt-16 lg:mt-20" />
-    
+    <HeroBanner class="animate-blurred-fade-in" />
+    <SearchInput v-model="searchQuery"
+      class="animate-blurred-fade-in mt-8 max-w-[80%] sm:max-w-[60%] xl:max-w-[30%] mb-44 sm:mt-16 lg:mt-20" />
+
     <div class="text-center gap-8 sm:gap-16 m-8 sm:m-20 mt-32 sm:mt-64">
       <h2 class="text-2xl sm:text-3xl font-bold mb-8 sm:-mb-20">
         Here you have some games you may like!
       </h2>
-      
+
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-8 sm:gap-16 2xl:gap-10 m-8 sm:m-36">
-        <GameCard 
-          v-for="game in randomGames" 
-          :key="game.name" 
-          :game="game"
-          class="w-full h-full"
-        />
+        <GameCard v-for="game in randomGames" :key="game.name" :game="game" class="w-full h-full" />
       </div>
     </div>
   </main>
