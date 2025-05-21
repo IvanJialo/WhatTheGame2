6c6f2015519835fd603a856abbc46525bdee27bf
IvanJialo <ivajimalo@alu.edu.gva.es>
2025-05-22 01:01:58 +0200

    PWA, logos y configuración basica para PWA

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,38 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'logo.png', 'logo-192.png', 'logo-512.png'],
+      manifest: {
+        name: 'WhatTheGame',
+        short_name: 'WhatTheGame',
+        description: 'Descubre cuánto duran los videojuegos y mucho más',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'logo-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
